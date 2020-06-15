7626edb322f68f50be6356a8de0cf6f6ea88eb5c
jihochoi <wlghsp@gmail.com>
2020-06-15 23:29:44 +0900

    benkyo

---
+++ jumptopython/gugu.py
@@ -0,0 +1,11 @@
+
+def GuGu(n):
+    result = []
+    i = 1
+    while i <10:
+        result.append(n*i)
+        i = i+1
+    return result
+
+print(GuGu(2))
+
---
+++ jumptopython/threeAndFive.py
@@ -0,0 +1,10 @@
+
+
+sum = 0
+for n in range(1,1000):
+    if n % 3 == 0 or n % 5 == 0:
+        sum += n 
+
+
+print(sum)
+
